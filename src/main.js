f7dfdab5541206b44c511ce4566e5337aed63042
jeffrey liu <jeffrey_lq@163.com>
2021-01-08 09:38:02 +0800

    init repository

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
+import 'lib-flexible'
 
-Vue.config.productionTip = false
+import router from "./router";
 
 new Vue({
+  // components: {
+  //   App
+  // },
+  // template: '<App/>'
   render: h => h(App),
+  router    // 所有组件都能看到 $router和$route  <router-link> 和 <router-view/>
 }).$mount('#app')
